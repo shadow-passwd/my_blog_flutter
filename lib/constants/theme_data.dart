624822830ab500ecbd11961c412cf048337b108c
shadow <shadowpass12@gmail.com>
2020-06-24 21:16:13 +0530

    changes

--- lib/constants/theme_data.dart
+++ lib/constants/theme_data.dart
@@ -15,7 +15,7 @@ class Styles {
       buttonColor: isDarkTheme ? Color(0xff3B3B3B) : Color(0xffF1F5FB),
       hintColor: isDarkTheme ? Color(0xff280C0B) : Color(0xffEECED3),
       highlightColor: isDarkTheme ? Color(0xff372901) : Color(0xffFCE192),
-      // hoverColor: isDarkTheme ? Color(0xff3A3A3B) : Color(0xff4285F4),
+      hoverColor: isDarkTheme ? Color(0xff3A3A3B) : Color(0xff4285F4),
       focusColor: isDarkTheme ? Color(0xff0B2512) : Color(0xffA8DAB5),
       disabledColor: Colors.grey,
       textSelectionColor: isDarkTheme ? Colors.white : Colors.black,
